d2b5668112ea24e77474be3d558d7d5b451b9c17
Oskar Georgsson <oskar.georgsson@gmail.com>
2024-10-01 18:18:12 +0200

    Temp

---
+++ GherkinLens/GherkinParser.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace GherkinLens;
+
+public static class GherkinParser
+{
+    public static void ReadAllFeaturesInAssembly()
+    {
+        var result = Assembly.LoadFile("/home/oskargeorgsson/repos/gherkin-lens/GherkinLens.Specifications/bin/Debug/net8.0/GherkinLens.Specifications.dll");
+
+        var resourceNames = result.GetManifestResourceNames();
+        var streams = result.GetManifestResourceStream(resourceNames.First());
+
+        
+
+    }
+}
